155b28e46c914c02cf98e1ac75731be187826a5b
ouadiamensar <amensarouadia1@gmail.com>
2026-06-30 18:34:26 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  base: import.meta.env.VITE_BASE_PATH || "/Clanopia" ,
   resolve: {
     alias: {
       'simple-peer': 'simple-peer/simplepeer.min.js'

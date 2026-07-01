bee109411330835913e25726917476c9fc7ad7a9
ouadiamensar <amensarouadia1@gmail.com>
2026-07-01 22:33:37 +0100

    chore: clean lockfile and update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        keep_fnames: true,
-      },
-    },
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('firebase')) {
             return 'firebase';
           }
-        },
-      },
-    },
-  },
+        }
+      }
+    }
+  }
 })

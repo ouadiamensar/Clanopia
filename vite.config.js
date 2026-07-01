583fd5df04a65e16408c1568e414dc2f08e90d2f
ouadiamensar <amensarouadia1@gmail.com>
2026-07-01 22:07:04 +0100

    fix: add esbuild config to prevent emotion crashe

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          // دمج حزمة فايربيس بشكل صريح لضمان عدم تشتت الدوال
           if (id.includes('firebase')) {
             return 'firebase';
           }
         }
       }
     }
+  },
+  // الأسطر الجديدة لحل مشكلة Emotion/MUI:
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+    jsxFactory: 'jsx',
   }
 })
